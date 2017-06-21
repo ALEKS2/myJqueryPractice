5835957817ce84aa26db019ad387d52dd3119d61
Aleks <aleksnyombi@gmail.com>
2017-06-21 13:08:36 -0700

    first part

---
+++ jquery/GIT.php
@@ -0,0 +1,10 @@
+<?php 
+/* GIT COMANDS*/
+# $git init //initialise local GIT Repository
+# $git add<file>  //Adds file(s) to index
+# $git status  //Checks status of working Tree
+# $git commit  //commits Changes in Index
+# $git pus     //push To Remote Respository
+# $git pull   //pull latest from remote repository
+#git clone  //clone Repository into a new Directory
+ ?>
